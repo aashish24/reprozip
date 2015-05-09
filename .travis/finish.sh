4fa633b8a3e1f1038b5318f73ca30c5a753ea66a
Remi Rampin <remirampin@gmail.com>
2015-05-09 13:37:48 -0400

    DEBUG: list .coverage files

--- .travis/finish.sh
+++ .travis/finish.sh
@@ -6,8 +6,10 @@ case "$TEST_MODE"
 in
     coverage)
         # Python
+        find . -name '.coverage*'
         if [ -f .coverage ]; then mv .coverage .coverage.orig; fi # FIXME: useless?
         coverage combine
+        find . -name '.coverage*'
         python -c "import coveralls.cli; coveralls.cli.main()"
 
         # C
